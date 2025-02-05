a0959dd885c73468cef1ba20440e6e246bbcfb10
ndbzika <flaviohenriquefc@gmail.com>
2025-02-05 20:15:32 -0300

    [FIX]: Hydratation error solution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${garetFont.className} antialiased`}>
+        <time dateTime="2016-10-25" suppressHydrationWarning />
         <AuthProvider>
           {children}
           <ToastContainer />
